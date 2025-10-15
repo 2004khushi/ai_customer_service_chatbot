2a39530a4d5c23055b6fa7dd205963d3b77bf8c9
2004khushi <goyalkhushi1004@gmail.com>
2025-10-15 15:45:58 +0530

    13:conversation

--- apps/widget/modules/widget/atoms/widget-atoms.ts
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -9,6 +9,7 @@ export const organizationIdAtom = atom<string | null> (null);
 
 export const errorMessageAtom = atom<string | null>(null);
 export const loadingMessageAtom = atom<string | null>(null);
+export const conversationIdAtom = atom<Id<"conversations"> | null>(null);
 
 export const contactSessionIdAtomFamily = atomFamily((organizationId: string) => {
     return atomWithStorage<Id<"contactSessions"> | null>(`${CONTACT_SESSION_KEY}_${organizationId}`, null) 
